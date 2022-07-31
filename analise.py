36eb7b09e7358e6d1bc8fb803087472950e5bb85
Leo-alves2 <leo.alves.b.vieira@gmail.com>
2022-07-31 20:29:24 -0300

    Importação de bibliotecas

---
+++ analise.py
@@ -0,0 +1,12 @@
+"""Sistema de recomendação usando Python e com IA"""
+
+# Instalar as bibliotecas recomendadas no arquivo requirements.txt
+
+# Importar as bibliotecas
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
